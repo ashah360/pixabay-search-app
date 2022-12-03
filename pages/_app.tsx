fe06cb7e60ee97f834117e465422a1de7f347ab4
Arman <armanshah360@gmail.com>
2022-12-03 10:21:29 -0800

    Add HoC for RTK query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '../store';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { api } from '@/store/api/api';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <ApiProvider api={api}>
+        <Component {...pageProps} />
+      </ApiProvider>
+    </Provider>
+  );
 }
