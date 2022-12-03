21e812e84c54c64ff84e337caadec051b0fbb7a8
Arman <armanshah360@gmail.com>
2022-12-03 10:50:10 -0800

    Inject theme context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,16 @@ import { Provider } from 'react-redux';
 import { store } from '../store';
 import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
 import { api } from '@/store/api/api';
+import { defaultTheme } from 'styles/theme';
+import { ThemeProvider } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <ApiProvider api={api}>
-        <Component {...pageProps} />
+        <ThemeProvider theme={defaultTheme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </ApiProvider>
     </Provider>
   );
